34fbd0ef2bd67bdf573239cc48c9a4c1d8af85fc
ChinCluBi <chincub@gmail.com>
2016-05-14 16:45:11 +0700

    Add scss loader

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,39 @@ module.exports = {
 				test: /\.(js|jsx)$/,
 				exclude: /node_modules/,
 				loader: 'babel'
-			}
+			},
+			{
+				test: /\.css$/,
+				exclude: /node_modules/,
+				loaders: [
+					'style-loader',
+					'css-loader'
+				]
+			},
+			{
+				test: /\.scss$/,
+				exlude: /node_modules/,
+				loaders: [
+					'style-loader',
+					{
+						loader: 'css-loader',
+						query: {
+							sourceMap: true
+						}
+					},
+					{
+						loader: 'sass-loader',
+						query: {
+							outputStyle: 'expanded',
+							sourceMap: true
+						}
+					}
+				]
+			},
+			'postcss-loader'
 		]
+	},
+	postcss: function () {
+		return [autoprefixer]
 	}
 }
