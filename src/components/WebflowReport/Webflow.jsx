77f790b97d58e17db803d65f4d3c9e8229d9f08c
ChinCluBi <chincub@gmail.com>
2016-05-15 20:32:19 +0700

    Add image in web flow page

--- src/components/WebflowReport/Webflow.jsx
+++ src/components/WebflowReport/Webflow.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 
 export default class Webflow extends React.Component {
     render() {
+    	var divMaxSize = {
+    		width: '100%'
+    	}
         return (
-            <div>
-                This is webflow report.
+        	<div className="ui segment basic" style={divMaxSize}>
+                <h1 className="ui header">
+                    <i className="fork icon"></i>
+                    <div className="content">
+                        Web Page Flow Report
+                    </div>
+                </h1>
+                <div className="ui divider"></div>
+                <img src="assets/imgs/funnel_full.png" alt=""/>
             </div>
         )
     }
